1c7fa660023d797bb9b066b27239d085c3c7399e
Jonathan Perry <jonathanperry@sequencemedia.net>
2022-12-21 21:44:17 +0000

    Declaration

---
+++ device.d.ts
@@ -0,0 +1,13 @@
+declare module '@sequencemedia/device' {
+  export const PORT: number
+  export const SECURE_PORT: number
+  export function getWindowLocationOrigin(context: globalThis): string
+  export function getWindowLocationProtocol(context: globalThis): string
+  export function getWindowLocationHostname(context: globalThis): string
+  export function getWindowLocationHost(context: globalThis): string
+  export function getWindowLocationPort(context: globalThis): string | number
+  export function getWindowLocationHref(context: globalThis): string
+  export function isSecure(context: globalThis): boolean
+  export function isDefaultPort(context: globalThis): boolean
+  export function isDefaultSecurePort(context: globalThis): boolean
+}
