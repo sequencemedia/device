3ce732c9f43412064b71759cf25f3ecc5b60bb11
Jonathan Perry <jonathanperry@sequencemedia.net>
2023-07-29 18:50:44 +0100

    `chai`

--- test/src/document.spec.cjs
+++ test/src/document.spec.cjs
@@ -1,19 +1,20 @@
 const {
-  expect
-} = require('chai')
-
-const {
   getWindowDocument
 } = require('@sequencemedia/device/document')
 
-describe('@sequencemedia/device', () => {
-  describe('`getWindowDocument`', () => it('is a function', () => expect(getWindowDocument).to.be.a('function')))
+import('chai')
+  .then(({
+    expect
+  }) => {
+    describe('@sequencemedia/device', () => {
+      describe('`getWindowDocument`', () => it('is a function', () => expect(getWindowDocument).to.be.a('function')))
 
-  describe('`getWindowDocument()`', () => {
-    const mockDocument = {}
+      describe('`getWindowDocument()`', () => {
+        const mockDocument = {}
 
-    describe('`document` is defined', () => it('returns an object', () => expect(getWindowDocument({ document: mockDocument })).to.equal(mockDocument)))
+        describe('`document` is defined', () => it('returns an object', () => expect(getWindowDocument({ document: mockDocument })).to.equal(mockDocument)))
 
-    describe('`document` is not defined', () => it('returns an object', () => expect(getWindowDocument()).to.eql({})))
+        describe('`document` is not defined', () => it('returns an object', () => expect(getWindowDocument()).to.eql({})))
+      })
+    })
   })
-})
